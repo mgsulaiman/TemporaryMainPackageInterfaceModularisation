7e641fa453f44e1c065ab2e6858a82e2377e5a43
mgsulaiman <info@mgsulaiman.com>
2024-04-13 13:45:37 +0300

    init

--- Package.swift
+++ Package.swift
@@ -3,21 +3,25 @@
 
 import PackageDescription
 
+private let commonModels = "CommonModels"
 let package = Package(
     name: "TemporaryMainPackageInterface",
+    platforms: [.iOS(.v17)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "TemporaryMainPackageInterface",
             targets: ["TemporaryMainPackageInterface"]),
     ],
+    dependencies: [
+        .package(name: commonModels, path: "../\(commonModels)"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "TemporaryMainPackageInterface"),
-        .testTarget(
-            name: "TemporaryMainPackageInterfaceTests",
-            dependencies: ["TemporaryMainPackageInterface"]),
+            name: "TemporaryMainPackageInterface", dependencies: [
+                .product(name: commonModels, package: commonModels)
+            ]),
     ]
 )
